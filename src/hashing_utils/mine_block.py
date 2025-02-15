cbb3efbcc26c65593fbd27190ec92b5e5bc92888
Maxwell Tang <amoinus@gmail.com>
2025-02-15 15:30:54 -0500

    Added C code

--- src/hashing_utils/mine_block.py
+++ src/hashing_utils/mine_block.py
@@ -1,5 +1,6 @@
 # Importing required module
 import subprocess
+import mine_cpu
 
 # blank 128 bit nonce
 message = 'pubkey\n\n00000000000000000000000000000000'
@@ -9,18 +10,19 @@ commit_id = subprocess.run(['git', 'log'], capture_output=True).stdout.decode('u
 commit_id = commit_id.split('\n')[0]
 commit_id = commit_id.split(' ')[1]
 
-print(commit_id)
+# print(commit_id)
 
 commit_block = subprocess.run(['git', 'cat-file', '-p', commit_id], capture_output=True).stdout
 
-print(commit_block)
+# print(commit_block)
 
 commit_data = b'commit ' + str(len(commit_block)).encode('ascii') + b'\0' + commit_block
 
-print(commit_data)
+# print(commit_data)
 
-process = subprocess.Popen(['./mine_cpu'], stdin=subprocess.PIPE)
+process = subprocess.Popen(['./mine_cpu'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 process.stdin.write(commit_data)
 process.stdin.flush()
 
+print(mine_cpu.mine(4))
--- src/hashing_utils/mine_cpu.c
+++ src/hashing_utils/mine_cpu.c
@@ -1,3 +1,5 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,7 +8,7 @@
 
 void print_digest(unsigned char hash[20]) {
     for(int i = 0; i < 20; i++) {
-        printf("%02x", hash[i]);
+        fprintf(stderr, "%02x", hash[i]);
     }
     printf("\n");
 }
@@ -22,7 +24,7 @@ typedef struct nonce_iterable {
 
 char *hex_chars = "0123456789abcdef";
 
-void mine(nonce new_nonce, char *block, uint32_t length, unsigned char hash[20]) {
+void mine_single(nonce new_nonce, char *block, uint32_t length, unsigned char hash[20]) {
     char *nonce_location = block + length - 33;
     for(int i = 0; i < 16; i++) {
         nonce_location[30 - i * 2] = hex_chars[(new_nonce.data[i] & 0xf0) >> 4];
@@ -30,13 +32,21 @@ void mine(nonce new_nonce, char *block, uint32_t length, unsigned char hash[20])
     }
     char *header = block;
     char *data = block + 1 + strlen(header);
-    printf("===header===\n%s\n", header);
-    printf("===data===\n%s\n===sha1 hash===\n", data);
+    fprintf(stderr, "===header===\n%s\n", header);
+    fprintf(stderr, "===data===\n%s\n===sha1 hash===\n", data);
 
     SHA1(hash, block, length);
 
     print_digest(hash);
-    printf("\n");
+    fprintf(stderr, "\n");
+}
+
+static PyObject *mine(PyObject *self, PyObject *args)
+{
+    int x;
+    if (!PyArg_ParseTuple(args, "i", &x))
+        return NULL;
+    return PyLong_FromLong(x * 2);
 }
 
 int main() {
@@ -59,17 +69,18 @@ int main() {
 
     print_digest(hash_out);
 
-    printf("Mining with template: \n");
-    printf("===header===\n%s\n", my_string);
-    printf("===data===\n%s\n===data===\n", data_block);
+    fprintf(stderr, "Mining with template: \n");
+    fprintf(stderr, "===header===\n%s\n", my_string);
+    fprintf(stderr, "===data===\n%s\n===data===\n", data_block);
 
     unsigned char hash[20];
     uint64_t i = 0;
-    while(hash[0] || hash[1] || hash[2] || hash[3]) {
+    while(hash[0] || hash[1]) {
         nonce_iterable my_nonce;
         my_nonce.l = i;
         my_nonce.h = 0;
-        mine(*((nonce *)&my_nonce), my_string, length, hash);
+        mine_single(*((nonce *)&my_nonce), my_string, length, hash);
         i++;
     }
+    printf("%s\n", data_block);
 }
