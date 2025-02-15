ba5fd4d762528f2c015a279ecd5cf4114afc088a
andOrlando <bennettgillig@gmail.com>
2025-02-15 16:49:50 -0500

    moves tests to tests/

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,10 +1,6 @@
-import logic
+import gitcoin.logic
 
-def run_all_tests():
-    validate_tnx_test()
-
-def validate_tnx_test():
-    print("Running Validate TNX Test -----")
+def test_validate_tnx_simple():
     #makes a state
     s = logic.State({}, [], None, None, None)
 
@@ -12,10 +8,6 @@ def validate_tnx_test():
     #return Tnx(info.pubkey, info.srcs, info.dests, info.mining_fee, info.signature, hash, prev_hash)
     tnx_1 = logic.Tnx("pubkey1", [], {"pubkey2": 10}, 1, "sig", "TNX1", "eep")
     s.tnxs[tnx_1.hash] = tnx_1
-    print(s)
     tnx_2 = logic.Tnx("pubkey2", ["TNX1"], {"pubkey3": 9}, 1, "sig", "TNX2", "eep")
-    print (f"adding 1 valid tnx works: {logic.validate_tnx(tnx_2, s)}")
+    assert logic.validate_tnx(tnx_2, s)
 
-
-if __name__ == "__main__":
-    run_all_tests()
