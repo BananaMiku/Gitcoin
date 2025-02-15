b0131d5062ce18a4f1abdfaff2091e768252eedf
Austin Henlotter <ahenlotter@umass.edu>
2025-02-15 10:29:33 -0500

    updated logic signatures

--- src/logic.py
+++ src/logic.py
@@ -1,13 +1,18 @@
 class tnx:
-    def __init__(self, hash_, prev_hash_, dests_, sources_):
+    def __init__(self, hash_, prev_hash_, dests_, amnts_, source_):
         self.hash = hash_
-        self.prev_hash = prev_hash_
-        self.dests = dests_
-        self.sources = sources_ #list of tnx hashes
+        self.prev_hash = prev_hash_ 
+        self.dests = dests_ #dests 
+        self.amnts = amnts_ #amounts per dest 
+        self.source = source #list of tnx hashes
 
 def validate_tnx(to_validate: tnx, tnx_set):
     assert(tnx != NULL)
-    for source in tnx.sources:
+    amt_to_spend
+    if source not in tnx_set:
+        #source should exist
+        return False;
+
         amt = 0 
         for 
 
