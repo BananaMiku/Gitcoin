4a61eacddf73e60f4281a7e32a17394ea52aae8a
Jun-L04 <LuJunyang04@gmail.com>
2025-02-15 19:51:46 -0500

    more parser
    gitcoin auto help
    pay command changes

--- gitcoin/run.py
+++ gitcoin/run.py
@@ -3,98 +3,101 @@ import subprocess
 from gitcoin.transact import make_keys
 
 # only integer transactions are allowed
-def dest_and_amt(input):
-    # try parsing argument for 'Pay' command
-    # print(input)
-    try: 
+
+
+def dest_and_amt_info(args):
+    # join all arguments into a single string
+    input = ' '.join(args)
+    #print(input)
+    try:
         args = input.split(' ')
     except Exception:
-        print("Invalid Argument Sequences for 'Pay' comand.")
-    
+        print("Invalid Argument Sequences for 'Pay' command.")
+
     for i in range(len(args)):
         # even index = destinations
-        if i % 2 == 0 and type(args[i]) != str:
+        if i % 2 == 0 and not args[i].isalpha():
             raise TypeError("Destination must be a string.")
-        if i % 2 != 0 and type(args[i]) != int:
+        if i % 2 != 0 and not args[i].isdigit():
+            print(args[i])
             raise TypeError("Amount must be an integer.")
-    
+
     return args
 
 
 def run():
     parser = argparse.ArgumentParser()
-    # subparser accepts only one option name (no short and long)
     subparsers = parser.add_subparsers(dest="command", help="sub-command help")
 
     # payment command
-    parser.add_argument('-p', "--pay", nargs='+', help="Paying a desintination a certain amount of Gitcoin with fess",
-                        type=dest_and_amt)
+    pay_parser = subparsers.add_parser(
+        "pay", help="Pay a destination a certain amount of Gitcoin")
+    pay_parser.add_argument("dest_and_amt", nargs='+',
+                            help="Paying a destination a certain amount of Gitcoin with fees")
 
     # subparser for remote command
     remote_parser = subparsers.add_parser("remote", help="git remote clone")
-    remote_subparsers = remote_parser.add_subparsers(dest="remote_action", help="sub-command help")
+    remote_subparsers = remote_parser.add_subparsers(
+        dest="remote_action", help="sub-command help")
 
-    # subparser remote add 
-    remote_add_parser = remote_subparsers.add_parser( "add", help="git remote add clone")
+    # subparser remote add
+    remote_add_parser = remote_subparsers.add_parser(
+        "add", help="git remote add clone")
     remote_add_parser.add_argument("name", help="Name of the remote", type=str)
     remote_add_parser.add_argument("url", help="URL of the remote", type=str)
+
     # subparser remote remove
-    remote_remove_parser = remote_subparsers.add_parser( "remove", help="git remote remove clone")
-    remote_remove_parser.add_argument("name", help="Name of the remote", type=str)
-
-    keypair_parser = subparsers.add_parser("keypair", help="generate or set keypairs")
-    keypair_subparsers = keypair_parser.add_subparsers(dest="keypair_action", help="idk")
-
-    keypair_subparsers.add_parser("generate", help="generate private key")
-    keypair_set_parser = keypair_subparsers.add_parser("set", help="set your private key")
-    keypair_set_parser.add_argument("privkey", help="your private key", type=str)
+    remote_remove_parser = remote_subparsers.add_parser(
+        "remove", help="git remote remove clone")
+    remote_remove_parser.add_argument(
+        "name", help="Name of the remote", type=str)
 
     # mine command
-    parser.add_argument("-m", "--mine", help="Mine Gitcoin, with love <3", action="store_true") 
+    mine_parser = subparsers.add_parser(
+        "mine", help="Mine Gitcoin, with love <3")
 
     # observer command
-    parser.add_argument("-o", "--observer", help="TAKE A LOOK AT YOUR OWN BLOCKCHAIN", action="store_true")
+    observer_parser = subparsers.add_parser(
+        "observer", help="Take a look at your own blockchain")
 
     args = parser.parse_args()
 
-    # not subcommand
-    if args.pay:
-        # try python main.py --pay A 1 B 2 C 33 40
-        payment_info = args.pay
-        # if args.pay is odd, the last element is the fee, default fee otherwise
+    # no input, print help message
+    print(vars(args))
+    if args.command is None:
+        parser.print_help()
+
+    if args.command == "pay":
+        print("pay")
+        payment_info = dest_and_amt_info(args.dest_and_amt)
         if len(payment_info) % 2 != 0:
-            fee = args.pay.pop(-1)
+            fee = payment_info.pop(-1)
         else:
             fee = 1
 
         print(f"fee is {fee}")
-        for i in range(0, len(payment_info), 2): # should be even in length
-            print(f"Destination: {payment_info[i]}, Amount: {payment_info[i+1]}")
+        for i in range(0, len(payment_info), 2):
+            print(
+                f"Destination: {payment_info[i]}, Amount: {payment_info[i+1]}")
 
-    # subcommand
-    if args.command == "remote":
+    elif args.command == "remote":
         print('remote')
         if args.remote_action == "add":
             print(f"Adding remote: {args.name} with URL: {args.url}")
-            subprocess.Popen(f'git remote add {args.name} {args.url}', shell=True)
+            subprocess.Popen(
+                f'git remote add {args.name} {args.url}', shell=True)
         elif args.remote_action == "remove":
             print(f"Removing remote: {args.name}")
             subprocess.Popen(f'git remote rm {args.name}', shell=True)
-
         else:
             raise Exception("Invalid remote command")
 
-    if args.command == "keypair":
-        if args.keypair_action == "set":
-            print(f"setting private key {args.privkey}")
-        if args.keypair_action == "generate":
-            [priv, pub] = make_keys()
-            print(f"keys:\nprivate {priv}\npublic: {pub}\n\nthese are saved")
-    
-    # mine command going thru
-    if args.mine:
+    elif args.command == "mine":
         print("Mining Gitcoin...")
 
-    # observer command going thru
-    if args.observer:
+    elif args.command == "observer":
         print("Observer placeholder")
+
+
+if __name__ == "__main__":
+    run()
